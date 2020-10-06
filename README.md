bed33b3c1f2c586e005aa715f46763156355622d
ahmedqamesh <ahmedqamesh@github.com>
2020-10-06 10:23:43 +0200

    guessinggame commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Peer-graded Assignment: Bash, Make, Git, and GitHub
-Tue Oct  6 10:14:46 CEST 2020
-1
+Tue Oct  6 10:23:27 CEST 2020
+41
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,41 @@
-echo "Hello"
+#!/usr/bin/env bash
+# File: guessinggame.sh  Author: Ahmed Qamesh
+# Aim: Guess the number of files in a directory
+
+allFiles = $(ls | wc -l)
+guessNr = 1
+
+patt="^[0-9]*$"
+max=100  #default value if input parameter is invalid
+if [[ $1 =~ $patt ]] && [[ $1 -gt allFiles ]]
+then
+	max=$1
+fi
+
+echo "Guess the number of files in the $PWD directory. Enter an integer between 0 and $max"
+while [[ $guess -ne $allFiles ]]
+do 
+	printf "Enter guess #$guessNr: "
+    read -r guess
+    if ([[ $guess =~ $patt ]] && [[ $guess -le $max ]] && [[ $guess -gt 0 ]])
+    then
+    if [[ $guess -lt allFiles ]]
+ 	then
+		echo -e "The guess of $guess is too low\n"
+		let guessNr=$guessNr+1
+	elif [[ $guess -gt allFiles ]]
+	then
+		echo -e "The guess of $guess is too high\n"
+		let guessNr=$guessNr+1
+	fi
+    else 
+        echo "$guess is an invalid value.  Please enter an integer between 0 and $max"
+    fi
+done
+
+if [[ guessNr -eq 1 ]] 
+then
+    echo -e "\nINCREDIBLE!!! You guessed correctly on your very first try!!\n"
+else   
+    echo -e "\nCongratulations! You guessed correctly with $guessNbr tries!!!\n"
+fi
